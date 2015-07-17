6f696c636ade00ed7da88502b34cd2a6dc81245b
delphiwu <delphi_wu@icloud.com>
2015-07-17 11:15:54 +0800

    Initial Commit

---
+++ Runner/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Runner
+//
+//  Created by delphiwu on 15/7/17.
+//  Copyright (c) 2015年 Tech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
